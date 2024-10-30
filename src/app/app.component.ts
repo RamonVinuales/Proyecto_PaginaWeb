659afd2449107ad6139f2de820c43b3be6b0dfd0
RamonVinuales <ramonito01@gmail.com>
2024-10-30 19:36:24 +0100

    Commit VIII

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { ListaTareasComponent } from './components/main/lista-tareas/lista-tarea
 import { TareasAddComponent } from "./components/main/tareas-add/tareas-add.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { MainComponent } from "./components/main/main.component";
+import { FooterComponent } from "./components/footer/footer/footer.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaTareasComponent, TareasAddComponent, RouterLink, HeaderComponent, MainComponent],
+  imports: [RouterOutlet, ListaTareasComponent, TareasAddComponent, RouterLink, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
